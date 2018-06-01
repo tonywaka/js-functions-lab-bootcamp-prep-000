9bfa28e1e08b880891dbfad734f5ae6ba026015e
Anthony Udezi <tonywaka@uniben.edu>
2018-06-01 21:11:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(name) {
   return 'Happy holidays, ${name}!';
 }
 function happyHolidayTo(holiday,name) {
-  var holiday = "holidays"
+  var holiday = "Independence Day"
   var name = "you"
   return `Happy ${holiday}, ${name}!`;
 }
